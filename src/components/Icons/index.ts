1c8cd13ef0ad7b816622bc2780098cb1c28ff2d4
TNattawutK <tomkabtokom@gmail.com>
2023-04-21 00:24:50 +0700

    feat: add component Icon

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,7 @@
+export { default as IconChevronUp } from "./IconChevronUp";
+export { default as IconColumn } from "./IconColumn";
+export { default as IconClose } from "./IconClose";
+export { default as IconFilter } from "./IconFilter";
+export { default as IconSpin } from "./IconSpin";
+export { default as IconTable } from "./IconTable";
+export { default as IconTicket } from "./IconTicket";
