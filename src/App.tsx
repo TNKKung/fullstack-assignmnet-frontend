75a49a8889db7400918d62aa8ef3e26814a8784b
TNattawutK <tomkabtokom@gmail.com>
2023-04-22 03:11:03 +0700

    fix: change to use component modal create ticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 
-import {
-  IconSpin,
-  IconTicket,
-  IconClose,
-  IconTable,
-  IconColumn,
-} from "./components/Icons";
-import { Modal } from "./components/Modal";
+import { IconTicket, IconTable, IconColumn } from "./components/Icons";
 import Incorporate from "./components/BoardTab";
 import { Tab, TabList, TabPanel, Tabs } from "./components/Tabs";
 import TableTab from "./components/TableTab";
 import { formatItem } from "./utils/formatItem";
-import { itemObject } from "./utils/type";
-
-interface listItemObject {
-  pending: itemObject[];
-  accepted: itemObject[];
-  resolved: itemObject[];
-  rejected: itemObject[];
-}
+import { itemObject, listItemObject } from "./utils/type";
+import ModalCreateTicket from "./components/ModalCreateTicket";
 
 function App() {
   const [isOpenCreateTicket, setIsOpenCreateTicket] = useState<boolean>(false);
@@ -43,27 +30,6 @@ function App() {
   const handleClickCloseModalCreateTicket = useCallback(() => {
     setIsOpenCreateTicket(false);
   }, []);
-
-  const handleInputTitle = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setTitle(e.target.value);
-    },
-    [setTitle]
-  );
-
-  const handleInputDescription = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      setDescription(e.target.value);
-    },
-    [setDescription]
-  );
-
-  const handleInputContact = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      setContact(e.target.value);
-    },
-    [setContact]
-  );
 
   const handleCreatedTicket = useCallback(() => {
     setIsLoading(true);
@@ -133,55 +99,15 @@ function App() {
         </Tabs>
       </div>
       {isOpenCreateTicket && (
-        <Modal isOpen={isOpenCreateTicket} size="md">
-          <div className="flex flex-col w-full h-full">
-            <div className="flex items-center justify-between">
-              Create Ticket
-              <button
-                className="p-1 text-red-500 border border-red-200 rounded-md hover:bg-red-500 hover:text-gray-50"
-                onClick={handleClickCloseModalCreateTicket}
-              >
-                <IconClose className="w-6 h-6" />
-              </button>
-            </div>
-            <div className="flex flex-col px-2 py-5 space-y-4 text-base">
-              <div className="space-y-1">
-                <p>Ticket Title</p>
-                <input
-                  onChange={handleInputTitle}
-                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none"
-                />
-              </div>
-              <div className="w-full h-full space-y-1">
-                <p>Description</p>
-                <textarea
-                  onChange={handleInputDescription}
-                  className="w-full h-56 p-2 border border-gray-300 rounded-md resize-none focus:outline-none"
-                />
-              </div>
-              <div className="w-full h-full space-y-1">
-                <p>Contract</p>
-                <textarea
-                  onChange={handleInputContact}
-                  className="w-full h-32 p-2 border border-gray-300 rounded-md resize-none focus:outline-none"
-                />
-              </div>
-            </div>
-            <div className="flex items-center justify-center w-full ">
-              <button
-                type="button"
-                className="w-24 p-2 space-x-1 text-green-700 border border-green-500 rounded-md bg-green-50 hover:bg-green-600 hover:text-gray-100"
-                onClick={handleCreatedTicket}
-              >
-                {isLoading ? (
-                  <IconSpin className="w-16 h-16 animate-spin" />
-                ) : (
-                  <p>Create</p>
-                )}
-              </button>
-            </div>
-          </div>
-        </Modal>
+        <ModalCreateTicket
+          isLoading={isLoading}
+          isOpenCreateTicket={isOpenCreateTicket}
+          setTitle={setTitle}
+          setDescription={setDescription}
+          setContact={setContact}
+          handleCreatedTicket={handleCreatedTicket}
+          handleClickCloseModalCreateTicket={handleClickCloseModalCreateTicket}
+        />
       )}
     </>
   );
