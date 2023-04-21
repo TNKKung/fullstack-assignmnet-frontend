f928cd0d06f64223761bef290086e2cb474b48fb
TNattawutK <tomkabtokom@gmail.com>
2023-04-22 01:53:40 +0700

    fix: border and bg color

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -10,9 +10,9 @@ interface ListProps {
 
 const List: React.FC<ListProps> = ({ children, title, name }) => {
   return (
-    <div className="flex flex-col w-1/4 h-full">
+    <div className="flex flex-col w-1/4 h-full shadow-lg">
       <h2 className="mx-3 mb-2 text-2xl font-bold">{title}</h2>
-      <div className="w-full py-2 overflow-y-auto border border-yellow-600 rounded-md h-5/6">
+      <div className="w-full py-2 overflow-y-auto border border-gray-800 rounded-md bg-gray-50 h-5/6">
         <Droppable droppableId={name}>
           {(provided: DroppableProvided) => (
             <div ref={provided.innerRef}>
