e65ee62a523262ab16526c2fa9eb9d615c51c8ea
TNattawutK <tomkabtokom@gmail.com>
2023-04-22 01:54:08 +0700

    fix: add border

--- src/components/Ticket/index.tsx
+++ src/components/Ticket/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Card: React.FC<Props> = ({ data }) => {
   return (
-    <div className="flex flex-row w-full h-24 p-4 border border-gray-400 rounded-md bg-gray-50">
+    <div className="flex flex-row w-full h-24 p-4 border border-gray-400 rounded-md shadow-lg bg-gray-50">
       <div className="flex items-center justify-center w-32 h-full rounded-md">
         <IconTicket className="w-12 h-12 text-gray-400" />
       </div>
